44d78f399e11636acd2f93b45d7f80bb214e8ece
Orville Bennett <dev@thebennettproject.com>
2023-10-28 01:48:15 -0400

    feat: Add command line options

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 # translocate - convert CSV translation files to JSON
 
-**`translocate`** is a high performance (it's *blazingly* fast) CSV translation to JSON translation file transformer. It takes `.csv` or `.tsv` files as input, and will output one file for each language listed as a CSV column.
+**`translocate`** is a high performance (*blazingly fast*) CSV translation file to JSON translation file transformer. It takes `.csv` or `.tsv` files as input, and will output one file for each language listed as a CSV column.
 
 ## Requirements for use
 
 There are some requirements for this crate to work properly:
   1. The input CSV (or TSV) file has a heading column.
 
-There are currently two internal methods used to transform your localized strings into JSON. The *fast* way only requires that CSV headers are present. The alternate method deserializes data based on a predefined list of languages. 
-  1. For this alternate mode the language indentifiers in the CSV should be in the format `xx_YY` or `xx-YY` &mdash; e.g. **en_US**.
-  2. This list includes the following languages: `da_DK`, `de_DE`, `en_US`, `es_ES`, `fr_FR`, `it_IT`, `nl_NL`, `pt_BR`, `pt_PT` and `sv_SE`.
+There are currently two internal methods used to transform your localized strings into JSON. The faster and more permissive method only requires that CSV headers are present. The alternate, stricter method deserializes data based on a predefined list of languages. For this alternate mode the language indentifiers in the CSV should:
+  1. be in the format `xx_YY` or `xx-YY` &mdash; e.g. **en_US**.
 
-If any of these requirements can not be met, interested users are encouraged to [make a pull request](https://code.orbitsolutions.dev/orb-it-solutions/translocate/pulls). Alternatively you may fork the repository and modify for your specific needs, as the license is *quite* permissive.
+The current list of languages is `da_DK`, `de_DE`, `en_US`, `es_ES`, `fr_FR`, `it_IT`, `nl_NL`, `pt_BR`, `pt_PT` and `sv_SE`.
 
-If you need to do some processing of your CSV before passing to **`translocate`**, consider checking out the [qsv](https://crates.io/crates/qsv) or [xsv](https://crates.io/crates/xsv) crate.
+**`translocate`** automatically tries the faster conversion method and, if it fails, will fall back to the stricter, slower strategy. If any of the requirements can not be met, interested users are encouraged to [make a pull request](https://code.orbitsolutions.dev/orb-it-solutions/translocate/pulls). Alternatively you may fork the repository and modify for your specific needs, as the license is *quite* permissive.
+
+If you need to do some processing of your CSV before passing to **`translocate`** because of failures during the transformation from CSV to JSON, consider checking out the [qsv](https://crates.io/crates/qsv) or [xsv](https://crates.io/crates/xsv) crate.
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,43 @@
+use argh::FromArgs;
 use csv::{Reader, ReaderBuilder, Terminator};
+use std::io;
 use std::path::PathBuf;
 use std::process;
-use std::{env, io};
 
 mod generators;
 mod translations;
 use generators::{generate_json, generate_json_fast};
 
-const MSG: &str = "Please give file path as a command line argument!";
+const APP_DESC: &str = "trans·lo·cate, verb, to move from one place to another.";
+
+#[derive(FromArgs)]
+/// High performance CSV translation to JSON translation file transformer.
+struct CliArgs {
+    #[argh(option, short = 'd')]
+    /// field delimiter to use when parsing. Uses `\t` for TSV and `,` for CSV by default.
+    delimiter: Option<u8>,
+    #[argh(option, short = 'e')]
+    /// escape character to use for quotes when parsing. Uses `\` for TSV and `"` for CSV by default.
+    escape_char: Option<u8>,
+    #[argh(switch, short = 'i')]
+    /// if the number of fields in records is allowed to change. Enabling makes parsing more strict.
+    inflexible: bool,
+    #[argh(option, short = 't')]
+    /// record terminator to use. CSV default is `\r`, `\n` or `\r\n`. TSV default is `\n`.
+    terminator: Option<u8>,
+    #[argh(switch, short = 'v')]
+    /// version info
+    version: Option<bool>,
+    #[argh(positional)]
+    /// relative or absolute path to CSV or TSV.
+    file: String,
+}
 
 /// Checks for and uses first argument as path to file. Prints error if no CLI argument given.
-fn get_file_location() -> Result<PathBuf, io::Error> {
+fn get_file_location(file: &str) -> Result<PathBuf, io::Error> {
     let cwd = std::env::current_dir()?;
     // Get cli arguments, then make sure an arg was actually passed
-    let path = env::args_os().nth(1).expect(MSG);
+    let path = file;
 
     let full_path = PathBuf::from(path);
 
@@ -25,7 +49,17 @@ fn get_file_location() -> Result<PathBuf, io::Error> {
 }
 
 fn main() -> Result<(), std::io::Error> {
-    let csv_path = if let Ok(path) = get_file_location() {
+    let cli: CliArgs = argh::from_env();
+    if let Some(_) = cli.version {
+        return Ok(println!(
+            "{} v{}\n{}",
+            env!("CARGO_PKG_NAME"),
+            env!("CARGO_PKG_VERSION"),
+            APP_DESC
+        ));
+    }
+
+    let csv_path = if let Ok(path) = get_file_location(&cli.file) {
         path
     } else {
         PathBuf::from("translations.csv")
@@ -41,9 +75,26 @@ fn main() -> Result<(), std::io::Error> {
         // If no file extension found, assume csv
         false
     };
-    let delimiter = if is_tsv { b'\t' } else { b',' };
-    let escape = if is_tsv { Some(b'\\') } else { Some(b'"') };
-    let terminator = if is_tsv {
+
+    let delimiter = if let Some(delim) = cli.delimiter {
+        delim
+    } else if is_tsv {
+        b'\t'
+    } else {
+        b','
+    };
+
+    let escape = if let Some(esc) = cli.escape_char {
+        Some(esc)
+    } else if is_tsv {
+        Some(b'\\')
+    } else {
+        Some(b'"')
+    };
+
+    let terminator = if let Some(terminate) = cli.terminator {
+        Terminator::Any(terminate)
+    } else if is_tsv {
         Terminator::Any(b'\n')
     } else {
         Terminator::CRLF
@@ -52,7 +103,7 @@ fn main() -> Result<(), std::io::Error> {
     let mut reader = ReaderBuilder::new()
         .delimiter(delimiter)
         .escape(escape)
-        .flexible(true)
+        .flexible(!cli.inflexible)
         .terminator(terminator)
         .from_path(&csv_path)?;
     let mut reader_count = Reader::from_path(&csv_path)?;
