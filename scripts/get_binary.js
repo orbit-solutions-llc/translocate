ebcf3e727d7a802e99dd0764a65cf3307a5b8d08
Orville Bennett <dev@thebennettproject.com>
2024-11-03 11:06:13 -0500

    feat: modify install directory

--- scripts/get_binary.js
+++ scripts/get_binary.js
@@ -1,12 +1,16 @@
 import { Binary } from "@orb/simple-binary-install"
 import pkg_info from '../package.json' with { type: 'json' }
 import os from 'node:os'
+import { dirname, join } from "node:path"
+import { fileURLToPath } from "node:url"
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 function getBinary() {
   const platform = getPlatform()
   const { name, version } = pkg_info
   const url = `https://github.com/orbit-solutions-llc/${name}/releases/download/v${version}/${name}-${platform}.tar.gz`
-  return new Binary(name, url);
+  return new Binary(name, url, { installDirectory: join(__dirname, "../bin") });
 }
 
 function getPlatform() {
